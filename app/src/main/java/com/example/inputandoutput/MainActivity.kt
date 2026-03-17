90c4e7ef4ce21ee0125c85167a3d6fff9acad32f
lwandilezengethwa-cmyk <lwandile.zengethwa@icloud.com>
2026-03-17 09:13:34 +0200

    added a switch button to switch from english to zulu and added code for the switch buttons using if statements

--- app/src/main/java/com/example/inputandoutput/MainActivity.kt
+++ app/src/main/java/com/example/inputandoutput/MainActivity.kt
@@ -4,15 +4,17 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Switch
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,11 +23,28 @@ class MainActivity : AppCompatActivity() {
         val welcomeText = findViewById<TextView>(R.id.displayText)
         val nameTxtField = findViewById<EditText>(R.id.nameText)
 // add code to button
-clickMeButton?.setOnClickListener {
-    Toast.makeText(this@MainActivity,"Button clicked",
-        Toast.LENGTH_LONG).show()
-    welcomeText.text = "Welcome ${nameTxtField.text}"
-    //welcomeTxt.text = "Welcome" * nameTxtField.text
+clickMeButton?.androidx.compose.foundation.layout.Box {
+    setOnClickListener {
+        Toast.makeText(
+            this@MainActivity, "Button clicked",
+            Toast.LENGTH_LONG
+        ).show()
+        welcomeText.text = "Welcome ${nameTxtField.text}"
+        //welcomeTxt.text = "Welcome" * nameTxtField.text
+
+        // switching between two languages
+        val zuluSwitch = findViewById<Switch>(R.id.zuluswitch)
+        // code to the button when it gets clicked
+        var greeting: String
+        if (zuluSwitch.isChecked) {
+            greeting = "Sawubona, ${nameTextField}!"
+        } else {
+            greeting = "Greetings, ${nameTextField}!"
+        }
+        WelcomeTextView.text = greeting
+
+
+    }
 }
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
