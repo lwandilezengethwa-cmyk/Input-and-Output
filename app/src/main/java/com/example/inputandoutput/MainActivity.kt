4c92976bea42cf6188d988de0ada86157a809a4e
lwandilezengethwa-cmyk <lwandile.zengethwa@icloud.com>
2026-03-18 09:17:03 +0200

    Added a boolean called zuluSwitch and added a nested loop for the boolean declared

--- app/src/main/java/com/example/inputandoutput/MainActivity.kt
+++ app/src/main/java/com/example/inputandoutput/MainActivity.kt
@@ -9,7 +9,6 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Switch
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -23,34 +22,46 @@ class MainActivity : AppCompatActivity() {
         val welcomeText = findViewById<TextView>(R.id.displayText)
         val nameTxtField = findViewById<EditText>(R.id.nameText)
 // add code to button
-clickMeButton?.androidx.compose.foundation.layout.Box {
-    setOnClickListener {
-        Toast.makeText(
-            this@MainActivity, "Button clicked",
-            Toast.LENGTH_LONG
-        ).show()
-        welcomeText.text = "Welcome ${nameTxtField.text}"
-        //welcomeTxt.text = "Welcome" * nameTxtField.text
+clickMeButton?.setOnClickListener {
+    Toast.makeText(
+        this@MainActivity, "Button clicked",
+        Toast.LENGTH_LONG
+    ).show()
+    welcomeText.text = "Welcome ${nameTxtField.text}"
+    //welcomeTxt.text = "Welcome" * nameTxtField.text
 
-        // switching between two languages
-        val zuluSwitch = findViewById<Switch>(R.id.zuluswitch)
-        // code to the button when it gets clicked
-        var greeting: String
-        if (zuluSwitch.isChecked) {
-            greeting = "Sawubona, ${nameTextField}!"
-        } else {
-            greeting = "Greetings, ${nameTextField}!"
+    // switching between two languages
+    val zuluSwitch = findViewById<Switch>(R.id.zuluSwitch)
+    // code to the button when it gets clicked
+    var greeting: String
+    val zulu: Boolean = zuluSwitch.isChecked
+
+    if (zuluSwitch.isChecked) {
+        greeting = "Sawubona, ${nameTxtField}!"
+    } else {
+        if (nameTxtField.text.toString() == "Sam" ||
+                nameTxtField.text.toString()== "Samantha"){
+            greeting = "Yo, ${nameTxtField.text}!"
         }
-        WelcomeTextView.text = greeting
-
-
-    }
+        else {
+            greeting = "Greetings, ${nameTxtField}!"
+        }
 }
-        setContentView(R.layout.activity_main)
+
+
+    welcomeText.text = greeting
+
+
+
+            setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
+            }
         }
     }
 }
+
